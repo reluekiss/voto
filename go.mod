a8983789eb55963e287be75551e13bf00b3ebb6f
relue <natalierogers048@gmail.com>
2024-07-21 22:59:45 -0400

    alsa attempt

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/reluekiss/voto
 
 go 1.22.5
 
-require github.com/ecobee/goalsa v0.0.0-20230731174733-19b297509b2e
-
-require github.com/cocoonlife/testify v0.0.0-20160218172820-792cc1faeb64 // indirect
+require (
+	github.com/cocoonlife/testify v0.0.0-20160218172820-792cc1faeb64 // indirect
+	github.com/nathan-hello/goalsa v0.0.0-20240721232453-2b6b84c2b478 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,65 @@
 package main
-import "github.com/ecobee/goalsa"
 
+import (
+	"fmt"
+	"log"
+	"os"
 
-func main() 
-{
-	alsa.	
+	alsa "github.com/nathan-hello/goalsa"
+) 
+
+func main() {
+    /* cmd := exec.Command("arecord", "--dump-hw-params")
+
+    // Execute the command and capture the output
+    output, err := cmd.CombinedOutput()
+    if err != nil {
+        fmt.Println("Error executing command:", err)
+        return
+    }
+
+    // Print the output
+    fmt.Println(string(output)) */
+	/* // Check hardware format capabilities
+	alsa.FormatS24LE
+	switch 
+		case :
+		8, 16, 24, 32, 64
+	// Check if x86
+	if {
+		format = alsa.FormatSXXLE
+	} else {
+		format = alsa.FormatSXXBE
+	} */
+    
+    var samples []byte
+
+    c, err := alsa.NewCaptureDevice("default", 2, alsa.FormatS24_3LE, 48000, alsa.BufferParams{})
+    if err != nil {
+	log.Fatal(err)
+    }
+    err = c.StartReadThread()
+    if err != nil {
+	log.Fatal(err)
+    }
+   
+    file, err := os.OpenFile("test.wav", os.O_CREATE | os.O_RDWR,0644)
+    if err != nil {
+	fmt.Println("Error opening file:", err)
+    }
+
+    for {
+	_, err = c.Read(samples)
+    	if err != nil {
+	    fmt.Println("Error reading from capture device:", err)
+	    break
+	}
+	_, err = file.Write(samples)
+    	if err != nil {
+	    fmt.Println("Error writing to file:", err)
+	    break
+	}
+    }
+    file.Close()
+    c.Close()
 }
