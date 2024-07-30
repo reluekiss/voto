403af5bcb9286c5c01df9bbef3f2e9daf84bf9c8
relue <natalierogers048@gmail.com>
2024-07-30 00:41:08 -0400

    trying to import github.com/xlab/opus-go with little success

--- main.go
+++ main.go
@@ -1,65 +1,122 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
-	alsa "github.com/nathan-hello/goalsa"
-) 
+	alsa "github.com/cocoonlife/goalsa"
+	opus "github.com/reluekiss/voto"
+)
 
-func main() {
-    /* cmd := exec.Command("arecord", "--dump-hw-params")
+/* cmd := exec.Command("arecord", "--dump-hw-params")
 
-    // Execute the command and capture the output
-    output, err := cmd.CombinedOutput()
-    if err != nil {
-        fmt.Println("Error executing command:", err)
-        return
-    }
+   // Execute the command and capture the output
+   output, err := cmd.CombinedOutput()
+   if err != nil {
+       fmt.Println("Error executing command:", err)
+       return
+   }
 
-    // Print the output
-    fmt.Println(string(output)) */
-	/* // Check hardware format capabilities
-	alsa.FormatS24LE
-	switch 
-		case :
-		8, 16, 24, 32, 64
-	// Check if x86
-	if {
-		format = alsa.FormatSXXLE
-	} else {
-		format = alsa.FormatSXXBE
-	} */
-    
-    var samples []byte
-
-    c, err := alsa.NewCaptureDevice("default", 2, alsa.FormatS24_3LE, 48000, alsa.BufferParams{})
+   // Print the output
+   fmt.Println(string(output)) */
+/* // Check hardware format capabilities
+alsa.FormatS24LE
+switch
+	case :
+	8, 16, 24, 32, 64
+// Check if x86
+if {
+	format = alsa.FormatSXXLE
+} else {
+	format = alsa.FormatSXXBE
+} */
+func captureaudio() []int16 {
+    c, err := alsa.NewCaptureDevice("default", 2, alsa.FormatS16LE, 48000, alsa.BufferParams{})
     if err != nil {
 	log.Fatal(err)
     }
+    defer c.Close()
+
     err = c.StartReadThread()
     if err != nil {
 	log.Fatal(err)
     }
-   
-    file, err := os.OpenFile("test.wav", os.O_CREATE | os.O_RDWR,0644)
+    
+    enc, err := opus.NewOpusEncoder(48000, 2, 20, 2)
     if err != nil {
-	fmt.Println("Error opening file:", err)
+        log.Fatal(err)
     }
+    defer enc.Close()
 
+    var samples, returnBuffer []int16
+    buffer := make([]byte, 2048)
+    r := bufio.NewReader(os.Stdin)
+    
     for {
+	input, _ := r.ReadString('\n')
+        input = strings.TrimSpace(input)
+
+        if string(input[0]) == "q" {
+            break
+        }
+
 	_, err = c.Read(samples)
     	if err != nil {
 	    fmt.Println("Error reading from capture device:", err)
 	    break
 	}
-	_, err = file.Write(samples)
-    	if err != nil {
-	    fmt.Println("Error writing to file:", err)
-	    break
-	}
+
+	_, err := enc.EncodeSamples(samples, len(samples), buffer)
+        if err != nil {
+            log.Fatal(err)
+        }
+	returnBuffer = append(returnBuffer, samples...)
     }
-    file.Close()
-    c.Close()
+    return returnBuffer
+}
+
+func playaudio(recordBuffer []int16) {
+    var length int
+    
+    p, err := alsa.NewPlaybackDevice("default", 2, alsa.FormatS16LE, 48000, alsa.BufferParams{})
+    if err != nil {
+	log.Fatal(err)
+    }
+    defer p.Close()
+    
+    dec, err := opus.NewOpusDecoder(48000, 2, 20)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer dec.Close()
+    
+   var buffer []byte 
+   for {
+   	_, err := dec.Decode(buffer[:length])
+   	if err != nil {
+   	    log.Fatal(err)
+   	}
+	p.Write(buffer)
+	if err != nil {
+    	    log.Fatal(err)
+    	}
+   } 
+}
+
+func main() {
+    var samples []int16
+    r := bufio.NewReader(os.Stdin)
+    
+    for {
+	input, _ := r.ReadString('\n')
+        input = strings.TrimSpace(input)
+
+        if string(input[0]) == "w" {
+	    samples = captureaudio()
+        }
+    	playaudio(samples)
+    }
 }
